62c1c0f9c6f859c3ac8bd86e38738e66cd55e257
Hide_D <hided62@gmail.com>
2025-12-28 16:43:33 +0000

    feat: vite-tsconfig-paths 추가로 타입 경로 설정 개선

--- app/game-engine/vitest.config.ts
+++ app/game-engine/vitest.config.ts
@@ -1,6 +1,14 @@
+import path from 'node:path';
+
 import { defineConfig } from 'vitest/config';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+    plugins: [
+        tsconfigPaths({
+            projects: [path.resolve(__dirname, '../../tsconfig.paths.json')],
+        }),
+    ],
     test: {
         environment: 'node',
         globals: true,
