d792069f65a6f6f06c46c8df625fdff22ff19a88
hided62 <hided62@gmail.com>
2026-08-12 15:48:47 +0000

    chore: update build toolchain dependencies

--- app/release-controller/vitest.config.ts
+++ app/release-controller/vitest.config.ts
@@ -1,10 +1,9 @@
-import path from 'node:path';
-
 import { defineConfig } from 'vitest/config';
-import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-    plugins: [tsconfigPaths({ projects: [path.resolve(__dirname, '../../tsconfig.paths.json')] })],
+    resolve: {
+        tsconfigPaths: true,
+    },
     test: {
         environment: 'node',
         include: ['test/**/*.test.ts'],
